35cd98952c080c905aaf45a081c3fe4a71401aba
HAOWEI960421 <haowei960421@outlook.com>
2021-05-02 15:00:52 -0400

    Revert "1"
    This reverts commit acc4c89d070eeb81eea69a0754c3b6e9a314dc8d.

--- dataset/__init__.py
+++
@@ -1,9 +0,0 @@
-from .vimeo90k import Vimeo90KDataset, VideoTestVimeo90KDataset
-from .mfqev2 import MFQEv2Dataset, VideoTestMFQEv2Dataset
-from .ChallengeDataset import ChallengeDataset, VideoTestChallengeDataset
-
-__all__ = [
-    'Vimeo90KDataset', 'VideoTestVimeo90KDataset',
-    'MFQEv2Dataset', 'VideoTestMFQEv2Dataset',
-    'ChallengeDataset', 'VideoTestChallengeDataset',
-    ]
